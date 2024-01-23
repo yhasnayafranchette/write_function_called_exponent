9347a5449184088c405719023e8fee377c47c779
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-23 11:57:34 +0800

    pseudocode

--- function_int_value_base_raised_exp.py
+++ function_int_value_base_raised_exp.py
@@ -1,2 +1,8 @@
 # Exercise 15: Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp.
 # Note here exp is a non-negative integer, and the base is an integer.
+
+#Check if exp is a non-negative integer
+
+#Print error message if exp is not a non-negative integer
+
+#Print the result
