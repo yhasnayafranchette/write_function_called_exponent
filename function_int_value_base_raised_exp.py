54bf7366642e31044fbde9e63233f28b4cd96168
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-23 12:11:00 +0800

    print base and exponent

--- function_int_value_base_raised_exp.py
+++ function_int_value_base_raised_exp.py
@@ -17,6 +17,8 @@ def exponent (base,exp):
 
 #Print the result
 base_value = 2
-exponent_value = 3
+exponent_value = 5
 result = exponent(base_value, exponent_value)
+print (f"base = {base_value}")
+print (f"exponent = {exponent_value}")
 print(f"{base_value} raised to the power of {exponent_value} is: {result}")
