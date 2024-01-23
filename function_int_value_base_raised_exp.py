1337e55ef3ff5d82946e283752ccb800f36671a9
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-23 12:17:41 +0800

    case 2, base 5 and exponent 4

--- function_int_value_base_raised_exp.py
+++ function_int_value_base_raised_exp.py
@@ -24,3 +24,12 @@ if result is not None:
     print (f"base = {base_value}")
     print (f"exponent = {exponent_value}")
     print(f"{base_value} raised to the power of {exponent_value} is: {result}")
+
+base_value = 5
+exponent_value = 4
+result = exponent(base_value, exponent_value)
+
+if result is not None:
+    print (f"base = {base_value}")
+    print (f"exponent = {exponent_value}")
+    print(f"{base_value} raised to the power of {exponent_value} is: {result}")
